7036c0a17734f0950cc4732cefa9c8f93e6b22b4
Hoang Hao <hoanghaosktt1ls@gmail.com>
2020-08-18 18:27:47 +0700

    add new

---
+++ contract.go
@@ -0,0 +1,21 @@
+package sdkmongo
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type MongoDBService interface {
+	GetClient() *mongo.Client
+	GetDatabase() *mongo.Database
+}
+
+type mongodbService struct {
+	client   *mongo.Client
+	database *mongo.Database
+}
+
+func (s *mongodbService) GetClient() *mongo.Client {
+	return s.client
+}
+
+func (s *mongodbService) GetDatabase() *mongo.Database {
+	return s.database
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/baozhenglab/mongodb
+
+go 1.13
+
+require (
+	github.com/baozhenglab/go-sdk v1.0.0
+	github.com/facebookgo/flagenv v0.0.0-20160425205200-fcd59fca7456 // indirect
+	go.mongodb.org/mongo-driver v1.4.0
+	go.opencensus.io v0.22.4 // indirect
+)
